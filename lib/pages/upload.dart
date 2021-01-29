2484d5e55bb94e8e004ab9052d8452eac44f4af3
ScottFarestrand <ScottFarestrand@gmail.com>
2021-01-28 20:22:13 -0800

    End of Lesson 29 Building uploadFrom .

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../models/user.dart';
@@ -129,6 +130,78 @@ class _UploadState extends State<Upload> {
                   color: Colors.blueAccent,
                   fontWeight: FontWeight.bold,
                   fontSize: 20.0),
+            ),
+          )
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 220.0,
+            width: MediaQuery.of(context).size.width * .8,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: FileImage(file),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 10.0)),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage:
+                  CachedNetworkImageProvider(widget.currentUser.photoUrl),
+            ),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Write a caption",
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(
+              Icons.pin_drop,
+              color: Colors.orange,
+              size: 36.0,
+            ),
+            title: Container(
+              width: 250,
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Where was this photo taken",
+                    border: InputBorder.none),
+              ),
+            ),
+          ),
+          Container(
+            height: 100.0,
+            width: 200.0,
+            alignment: Alignment.center,
+            child: RaisedButton.icon(
+              onPressed: () => print('Get Locations'),
+              icon: Icon(Icons.my_location),
+              label: Text(
+                'Use Current Location',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              color: Colors.blue,
             ),
           )
         ],
