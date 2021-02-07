0c515975e1697fd2516c0d1f4a4cb09fb3bcbac1
ScottFarestrand <ScottFarestrand@gmail.com>
2021-02-07 06:55:32 -0800

    Lesson 52 Function Deployed - May be second pass at this due to time line missing

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -8,6 +8,14 @@ module.exports = {
     "eslint:recommended",
     "google",
   ],
+  parserOptions: {
+    "sourceType": "module",
+    "ecmaVersion": 2020,
+    "ecmaFeatures": {
+      "jsx": true,
+      "experimentalObjectRestSpread": true,
+    },
+  },
   rules: {
     quotes: ["error", "double"],
   },
