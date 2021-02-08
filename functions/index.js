465e2e9435eba18fa619c6ecb1a0f88f43cf1e58
ScottFarestrand <ScottFarestrand@gmail.com>
2021-02-07 17:05:25 -0800

    Lesson 53 added delete function

--- functions/index.js
+++ functions/index.js
@@ -15,7 +15,9 @@ exports.onCreateFollower = functions.firestore
     .onCreate(async (snapshot, context) => {
       console.log("Follower Created", snapshot.id);
       const userId = context.params.userId;
+      // console.log(userId);
       const followerId = context.params.followerId;
+      // console.log(followerId);
 
       // 1) Create followed users posts ref
       const followedUserPostsRef = admin
@@ -24,22 +26,55 @@ exports.onCreateFollower = functions.firestore
           .doc(userId)
           .collection("userPosts");
 
+
       // 2) Create following user's timeline ref
       const timelinePostsRef = admin
           .firestore()
           .collection("timeline")
           .doc(followerId)
           .collection("timelinePosts");
+      // console.log("timeline ref created follower ID:", followerId);
 
       // 3) Get followed users posts
       const querySnapshot = await followedUserPostsRef.get();
+      // console.log("Snapshot Created");
 
       // 4) Add each user post to following user's timeline
+      // console.log("checking docs");
       querySnapshot.forEach((doc) => {
+        // console.log("processing Docs");
         if (doc.exists) {
+          console.log("processing Doc");
           const postId = doc.id;
           const postData = doc.data();
           timelinePostsRef.doc(postId).set(postData);
+          console.log("Timeline Data Added");
+        }
+      });
+    });
+
+
+exports.onDeleteollower = functions.firestore
+    .document("/followers/{userId}/userFollowers/{followerId}")
+    .onDelete(async (snapshot, context) => {
+      console.log("Follower Created", snapshot.id);
+      const userId = context.params.userId;
+      const followerId = context.params.followerId;
+      console.log("User ID:", userId);
+      const timelinePostsRef = admin
+          .firestore()
+          .collection("timeline")
+          .doc(followerId)
+          .collection("timelinePosts")
+          .where("ownerId", "==", userId);
+
+      const querySnapshot = await timelinePostsRef.get();
+
+      console.log("Got Documents");
+      querySnapshot.forEach((doc) => {
+        console.log("deleting doc: ", doc.id);
+        if (doc.exists) {
+          doc.ref.delete();
         }
       });
     });
